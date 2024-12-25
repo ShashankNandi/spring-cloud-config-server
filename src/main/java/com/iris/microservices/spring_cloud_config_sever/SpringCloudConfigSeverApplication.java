b18dc8974dfe9f37da53c9c6a5c3b46a847afaab
shashank189612 <shashanknm18@gmail.com>
2024-12-25 14:05:44 +0530

    added description message for the url

--- src/main/java/com/iris/microservices/spring_cloud_config_sever/SpringCloudConfigSeverApplication.java
+++ src/main/java/com/iris/microservices/spring_cloud_config_sever/SpringCloudConfigSeverApplication.java
@@ -11,5 +11,9 @@ public class SpringCloudConfigSeverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigSeverApplication.class, args);
 	}
+//	url to get the configuration "localhost:8888//limits-services/default"
+
+//	ie limits-services is the name of the propertied file in the local git repo
+//	default means the default environment , where other other environments can be production env, test env
 
 }
