b23d5b3835815e90c8d4cf33ea881aba155463dd
shashank189612 <shashanknm18@gmail.com>
2024-12-25 13:56:36 +0530

    connected the config server to the local git repo

---
+++ src/main/java/com/iris/microservices/spring_cloud_config_sever/SpringCloudConfigSeverApplication.java
@@ -0,0 +1,15 @@
+package com.iris.microservices.spring_cloud_config_sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigSeverApplication.class, args);
+	}
+
+}
